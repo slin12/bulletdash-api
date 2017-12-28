a05732d6f1e768ab6d2a5f98359b1a551d04c495
Shirley <s.lin12@gmail.com>
2017-12-28 17:46:17 -0500

    add uniqueness validation to trackers for each user

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,8 +1,9 @@
 class Tracker < ApplicationRecord
   belongs_to :user
+  validates :date_format, uniqueness: { scope: :user_id }
 
   def self.get_last_seven(user)
-    user.trackers.sort_by {|tracker| tracker.date_format}.first(7)
+    user.trackers.sort_by {|tracker| tracker.date_format}.last(7)
   end
 
 end
