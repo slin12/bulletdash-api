f3305c21c89c92360c306c0b6407b3672dd6bba5
Shirley <s.lin12@gmail.com>
2018-01-02 13:19:59 -0500

    notes bug fix

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,15 +4,10 @@ class NotesController < ApplicationController
     render json: @notes
   end
 
-  def create
-  end
-
   def update
     note = Note.find {|note| note.user === current_user} || Note.create(user: current_user)
     note.content = params['note']['content']
     note.save
   end
 
-  def destroy
-  end
 end
